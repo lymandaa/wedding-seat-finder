976f1281c2b9a44108248a363177b63ee62958df
lymandaa <amanda.ly821@gmail.com>
2026-07-19 20:51:04 +1000

    Create app

---
+++ app.js
@@ -0,0 +1,181 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbwHNdTlDchoC6JMk-_-4kFQBZ2ir7sgksbBJV79KHCDI1VvIVLw_Y_6UMHTQBy6ys146A/exec";
+
+
+const input = document.getElementById("guestName");
+const button = document.querySelector("button");
+
+let selectedGuest = "";
+
+
+// =============================
+// AUTOCOMPLETE
+// =============================
+
+input.addEventListener("input", async () => {
+
+    const search = input.value.trim();
+
+
+    if(search.length < 3){
+        return;
+    }
+
+
+    const response = await fetch(
+        `${API_URL}?search=${search}`
+    );
+
+
+    const results = await response.json();
+
+
+    showSuggestions(results);
+
+});
+
+
+
+
+// =============================
+// SHOW NAME OPTIONS
+// =============================
+
+function showSuggestions(results){
+
+
+    let oldList = document.querySelector(".suggestions");
+
+    if(oldList){
+        oldList.remove();
+    }
+
+
+    if(results.length === 0){
+        return;
+    }
+
+
+
+    const list = document.createElement("div");
+
+    list.className = "suggestions";
+
+
+    results.forEach(person => {
+
+
+        const item = document.createElement("div");
+
+        item.innerText = person.name;
+
+
+        item.addEventListener("click",()=>{
+
+            input.value = person.name;
+
+            selectedGuest = person.name;
+
+            list.remove();
+
+        });
+
+
+        list.appendChild(item);
+
+    });
+
+
+
+    input.parentElement.appendChild(list);
+
+}
+
+
+
+// =============================
+// FIND TABLE
+// =============================
+
+
+button.addEventListener("click", async ()=>{
+
+
+    if(!selectedGuest){
+
+        alert("Please select your name");
+
+        return;
+
+    }
+
+
+
+    const response = await fetch(
+        API_URL,
+        {
+
+            method:"POST",
+
+            body:JSON.stringify({
+
+                name:selectedGuest
+
+            })
+
+        }
+    );
+
+
+
+    const data = await response.json();
+
+
+
+    if(data.found){
+
+
+        document.querySelector(".result").innerHTML = `
+
+        <div class="guest-name">
+        ${data.guest}
+        </div>
+
+
+        <div class="divider"></div>
+
+
+        <div class="label">
+        YOUR TABLE
+        </div>
+
+
+        <div class="number">
+        ${data.table}
+        </div>
+
+
+
+        <div class="label">
+        SEATED WITH
+        </div>
+
+
+        <div class="party">
+
+        ${data.party
+            .filter(name => name !== data.guest)
+            .join("<br>")}
+
+        </div>
+
+
+        <p class="closing">
+        Enjoy the evening
+        </p>
+
+        `;
+
+
+    }
+
+});
